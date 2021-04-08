bd58a504a10ee5cea5ecfb47ba3b785a118fc4a7
Felipe Jung <felipemattoseu@gmail.com>
2021-04-08 12:12:14 -0400

    Add form validation to signIn page

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,20 +1,29 @@
+import { forwardRef } from "react";
+import { FieldError } from "react-hook-form";
+
 import {
   Input as ChakraInput,
   FormLabel,
   FormControl,
+  FormErrorMessage,
   InputProps as ChakraInputProps,
 } from "@chakra-ui/react";
 
 type InputProps = {
   name: string;
   label?: string;
+  error?: FieldError;
 } & ChakraInputProps;
 
-export const Input = ({ name, label, ...rest }: InputProps) => (
-  <FormControl>
+const InputBase: React.ForwardRefRenderFunction<
+  HTMLInputElement,
+  InputProps
+> = ({ name, label, error, ...rest }: InputProps, ref) => (
+  <FormControl isInvalid={!!error}>
     {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
 
     <ChakraInput
+      ref={ref}
       id={name}
       name={name}
       focusBorderColor="pink.500"
@@ -26,5 +35,9 @@ export const Input = ({ name, label, ...rest }: InputProps) => (
       size="lg"
       {...rest}
     />
+
+    {!!error && <FormErrorMessage>{error.message}</FormErrorMessage>}
   </FormControl>
 );
+
+export const Input = forwardRef(InputBase);
