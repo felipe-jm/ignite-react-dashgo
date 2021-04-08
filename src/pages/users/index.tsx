bcf381c360b8831926ac750dc5d2754ca37d8d18
Felipe Jung <felipemattoseu@gmail.com>
2021-04-08 11:32:12 -0400

    Make users list page responsive

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -12,6 +12,7 @@ import {
   Td,
   Checkbox,
   Text,
+  useBreakpointValue,
 } from "@chakra-ui/react";
 
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
@@ -20,74 +21,83 @@ import { Sidebar } from "components/Sidebar";
 import { Header } from "components/Header";
 import { Pagination } from "components/Pagination";
 
-const ListUsers = () => (
-  <Box>
-    <Header />
+const ListUsers = () => {
+  const isDesktopOrTable = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
 
-    <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
-      <Sidebar />
+  return (
+    <Box>
+      <Header />
 
-      <Box flex="1" borderRadius={8} bg="gray.800" p="8">
-        <Flex mb="8" justify="space-between" align="center">
-          <Heading size="lg" fontWeight="normal">
-            Usuários
-          </Heading>
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        <Sidebar />
 
-          <Button
-            as="a"
-            size="sm"
-            fontSize="sm"
-            colorScheme="pink"
-            leftIcon={<Icon as={RiAddLine} fontSize="20" />}
-          >
-            Criar novo
-          </Button>
-        </Flex>
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+          <Flex mb="8" justify="space-between" align="center">
+            <Heading size="lg" fontWeight="normal">
+              Usuários
+            </Heading>
 
-        <Table colorScheme="whiteAlpha">
-          <Thead>
-            <Tr>
-              <Th px="6" color="gray.300" width="8">
-                <Checkbox colorScheme="pink" />
-              </Th>
-              <Th>Usuário</Th>
-              <Th>Data de cadastro</Th>
-              <Th width="8"></Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            <Tr>
-              <Td px="6">
-                <Checkbox colorScheme="pink" />
-              </Td>
-              <Td>
-                <Box>
-                  <Text fontWeight="bold">Felipe Jung</Text>
-                  <Text fontSize="sm" color="gray.300">
-                    felipemattoseu@gmail.com
-                  </Text>
-                </Box>
-              </Td>
-              <Td>27 de janeiro, 200</Td>
-              <Td>
-                <Button
-                  as="a"
-                  size="sm"
-                  fontSize="sm"
-                  colorScheme="purple"
-                  leftIcon={<Icon as={RiPencilLine} fontSize="20" />}
-                >
-                  Editar
-                </Button>
-              </Td>
-            </Tr>
-          </Tbody>
-        </Table>
+            <Button
+              as="a"
+              size="sm"
+              fontSize="sm"
+              colorScheme="pink"
+              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+            >
+              Criar novo
+            </Button>
+          </Flex>
 
-        <Pagination />
-      </Box>
-    </Flex>
-  </Box>
-);
+          <Table colorScheme="whiteAlpha">
+            <Thead>
+              <Tr>
+                <Th px="6" color="gray.300" width="8">
+                  <Checkbox colorScheme="pink" />
+                </Th>
+                <Th>Usuário</Th>
+                {isDesktopOrTable && <Th>Data de cadastro</Th>}
+                <Th width="8"></Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td px={["4", "4", "6"]}>
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Felipe Jung</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      felipemattoseu@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                {isDesktopOrTable && <Td>27 de janeiro, 200</Td>}
+                {isDesktopOrTable && (
+                  <Td>
+                    <Button
+                      as="a"
+                      size="sm"
+                      fontSize="sm"
+                      colorScheme="purple"
+                      leftIcon={<Icon as={RiPencilLine} fontSize="20" />}
+                    >
+                      Editar
+                    </Button>
+                  </Td>
+                )}
+              </Tr>
+            </Tbody>
+          </Table>
+
+          <Pagination />
+        </Box>
+      </Flex>
+    </Box>
+  );
+};
 
 export default ListUsers;
