9ddda24f58066ca99e60d4fbfd82ba6ba05209c1
Felipe Jung <felipemattoseu@gmail.com>
2021-04-16 12:09:30 -0400

    Add react-query and fetch users

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -15,7 +15,10 @@ import {
   Checkbox,
   Text,
   useBreakpointValue,
+  Spinner,
 } from "@chakra-ui/react";
+
+import { useQuery } from "react-query";
 
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 
@@ -24,6 +27,22 @@ import { Header } from "components/Header";
 import { Pagination } from "components/Pagination";
 
 const ListUsers = () => {
+  const { data, isLoading, error } = useQuery("users", async () => {
+    const response = await fetch("http://localhost:3000/api/users");
+    const data = await response.json();
+
+    const users = data.users.map((user) => ({
+      ...user,
+      createdAt: new Date(user.createdAt).toLocaleDateString("pt-BR", {
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+      }),
+    }));
+
+    return users;
+  });
+
   const isDesktopOrTable = useBreakpointValue({
     base: false,
     lg: true,
@@ -55,49 +74,63 @@ const ListUsers = () => {
             </Link>
           </Flex>
 
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px="6" color="gray.300" width="8">
-                  <Checkbox colorScheme="pink" />
-                </Th>
-                <Th>Usuário</Th>
-                {isDesktopOrTable && <Th>Data de cadastro</Th>}
-                <Th width="8"></Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Felipe Jung</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      felipemattoseu@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isDesktopOrTable && <Td>27 de janeiro, 200</Td>}
-                {isDesktopOrTable && (
-                  <Td>
-                    <Button
-                      as="a"
-                      size="sm"
-                      fontSize="sm"
-                      colorScheme="purple"
-                      leftIcon={<Icon as={RiPencilLine} fontSize="20" />}
-                    >
-                      Editar
-                    </Button>
-                  </Td>
-                )}
-              </Tr>
-            </Tbody>
-          </Table>
+          {isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justify="center">
+              <Text>Falha ao obter dados do usuários</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme="whiteAlpha">
+                <Thead>
+                  <Tr>
+                    <Th px="6" color="gray.300" width="8">
+                      <Checkbox colorScheme="pink" />
+                    </Th>
+                    <Th>Usuário</Th>
+                    {isDesktopOrTable && <Th>Data de cadastro</Th>}
+                    <Th width="8"></Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {data.map((user) => (
+                    <Tr key={user.id}>
+                      <Td px={["4", "4", "6"]}>
+                        <Checkbox colorScheme="pink" />
+                      </Td>
+                      <Td>
+                        <Box>
+                          <Text fontWeight="bold">{user.name}</Text>
+                          <Text fontSize="sm" color="gray.300">
+                            {user.email}
+                          </Text>
+                        </Box>
+                      </Td>
+                      {isDesktopOrTable && <Td>{user.createdAt}</Td>}
+                      {isDesktopOrTable && (
+                        <Td>
+                          <Button
+                            as="a"
+                            size="sm"
+                            fontSize="sm"
+                            colorScheme="purple"
+                            leftIcon={<Icon as={RiPencilLine} fontSize="20" />}
+                          >
+                            Editar
+                          </Button>
+                        </Td>
+                      )}
+                    </Tr>
+                  ))}
+                </Tbody>
+              </Table>
 
-          <Pagination />
+              <Pagination />
+            </>
+          )}
         </Box>
       </Flex>
     </Box>
