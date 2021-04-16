81d6db9d6c078685761fda6edc9bbd37565e3520
Felipe Jung <felipemattoseu@gmail.com>
2021-04-16 13:23:48 -0400

    Add initial Pagination logic

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,25 +1,43 @@
 import { Stack, Button, Box } from "@chakra-ui/react";
 import { PaginationItem } from "./PaginationItem";
 
-export const Pagination = () => (
-  <Stack
-    direction={["column", "row"]}
-    mt="8"
-    justify="space-between"
-    align="center"
-    spacing="6"
-  >
-    <Box>
-      <strong>0</strong> - <strong>10</strong> de <strong>100</strong>
-    </Box>
+type PaginationProps = {
+  totalCountOfRegisters: number;
+  registersPerPage?: number;
+  currentPage?: number;
+  onPageChange: (page: number) => void;
+};
 
-    <Stack direction="row" spacing="2">
-      <PaginationItem number={1} isCurrent />
-      <PaginationItem number={2} />
-      <PaginationItem number={3} />
-      <PaginationItem number={4} />
-      <PaginationItem number={5} />
-      <PaginationItem number={6} />
+const siblingsCount = 1;
+
+export const Pagination = ({
+  totalCountOfRegisters,
+  registersPerPage = 10,
+  currentPage = 1,
+  onPageChange,
+}: PaginationProps) => {
+  const lastPage = Math.floor(totalCountOfRegisters / registersPerPage);
+
+  return (
+    <Stack
+      direction={["column", "row"]}
+      mt="8"
+      justify="space-between"
+      align="center"
+      spacing="6"
+    >
+      <Box>
+        <strong>0</strong> - <strong>10</strong> de <strong>100</strong>
+      </Box>
+
+      <Stack direction="row" spacing="2">
+        <PaginationItem number={1} isCurrent />
+        <PaginationItem number={2} />
+        <PaginationItem number={3} />
+        <PaginationItem number={4} />
+        <PaginationItem number={5} />
+        <PaginationItem number={6} />
+      </Stack>
     </Stack>
-  </Stack>
-);
+  );
+};
