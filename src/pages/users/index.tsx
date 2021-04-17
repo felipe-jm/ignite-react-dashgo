c94a796b390ed73e24bce38cb508f1919f492bea
Felipe Jung <felipemattoseu@gmail.com>
2021-04-17 10:54:08 -0400

    Add pagination to users list

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import Link from "next/link";
 
 import {
@@ -24,10 +26,11 @@ import { Sidebar } from "components/Sidebar";
 import { Header } from "components/Header";
 import { Pagination } from "components/Pagination";
 
-import { useUsers } from "services/hooks/userUsers";
+import { useUsers } from "services/hooks/useUsers";
 
 const ListUsers = () => {
-  const { data, isLoading, isFetching, error } = useUsers();
+  const [page, setPage] = useState(1);
+  const { data, isLoading, isFetching, error } = useUsers(page);
 
   const isDesktopOrTable = useBreakpointValue({
     base: false,
@@ -85,7 +88,7 @@ const ListUsers = () => {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  {data.map((user) => (
+                  {data.users.map((user) => (
                     <Tr key={user.id}>
                       <Td px={["4", "4", "6"]}>
                         <Checkbox colorScheme="pink" />
@@ -117,7 +120,11 @@ const ListUsers = () => {
                 </Tbody>
               </Table>
 
-              <Pagination />
+              <Pagination
+                totalCountOfRegisters={data.totalCount}
+                currentPage={page}
+                onPageChange={setPage}
+              />
             </>
           )}
         </Box>
